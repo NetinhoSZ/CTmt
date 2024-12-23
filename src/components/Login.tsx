ba893a5b4dca5e1789e79983beb6f037de5a69c7
NetinhoSZ <netinhoskennedy@gmail.com>
2024-12-23 00:37:35 -0300

    Second Commit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -14,23 +14,23 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
   };
 
   return (
-    <div className="w-full">
-      <div className="">
-        <h1 className="">Login</h1>
-        <form onSubmit={handleSubmit}>
+    <div className="w-full h-full flex items-center justify-center text-center mt-[50%] md:mt-[18%]">
+      <div className="max-w-full h-full bg-white p-10 rounded-xl">
+        <h1 className="w-full text-black mb-10">Login</h1>
+        <form className="w-full h-full flex flex-col gap-5" onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Nome de usuário"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className=""
+            className="px-5 py-2 rounded-lg outline-none"
           />
           <input
             type="password"
             placeholder="Senha"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className=""
+            className="px-5 py-2 rounded-lg outline-none"
           />
           <button
             type="submit"
